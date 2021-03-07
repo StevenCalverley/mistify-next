fde214dc0a7779d400ea823b9f64cb9759b8cfa6
Steve Calverley <steve.calverley@heffron.com.au>
2021-03-07 15:34:01 +1100

    Add Button Type

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,36 @@
   <main class="mt-4">
     <div class="max-w-7xl mx-auto">
       <section class="bg-white p-4 rounded">
-        <h1 class="text-lg font-medium">Buttons</h1>
-        <div class="flex space-x-8 items-end">
-          <m-button type="xs">Button Text</m-button>
-          <m-button type="sm">Button Text</m-button>
-          <m-button>Button Text</m-button>
-          <m-button type="lg">Button Text</m-button>
-          <m-button type="xl">Button Text</m-button>
+        <h1 class="text-lg font-bold">Buttons</h1>
+        <div class="mt-3 pt-4 border-t">
+          <h2 class="font-semibold">Primary</h2>
+          <div class="flex space-x-8 items-end">
+            <m-button size="xs">Button Text</m-button>
+            <m-button size="sm">Button Text</m-button>
+            <m-button>Button Text</m-button>
+            <m-button size="lg">Button Text</m-button>
+            <m-button size="xl">Button Text</m-button>
+          </div>
+        </div>
+        <div class="mt-4 pt-4 border-t">
+          <h2 class="font-semibold">Secondary</h2>
+          <div class="flex space-x-8 items-end">
+            <m-button size="xs" type="secondary">Button Text</m-button>
+            <m-button size="sm" type="secondary">Button Text</m-button>
+            <m-button type="secondary">Button Text</m-button>
+            <m-button size="lg" type="secondary">Button Text</m-button>
+            <m-button size="xl" type="secondary">Button Text</m-button>
+          </div>
+        </div>
+        <div class="mt-4 pt-4 border-t">
+          <h2 class="font-semibold">White</h2>
+          <div class="flex space-x-8 items-end">
+            <m-button size="xs" type="white">Button Text</m-button>
+            <m-button size="sm" type="white">Button Text</m-button>
+            <m-button type="white">Button Text</m-button>
+            <m-button size="lg" type="white">Button Text</m-button>
+            <m-button size="xl" type="white">Button Text</m-button>
+          </div>
         </div>
       </section>
     </div>
--- src/components/MButton/MButton.vue
+++ src/components/MButton/MButton.vue
@@ -1,8 +1,8 @@
 <template>
   <button
     type="button"
-    class="inline-flex items-center font-medium border border-transparent rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-    :class="typeClasses"
+    class="inline-flex items-center font-medium border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+    :class="[sizeClasses, typeClasses]"
     @click="$emit('click')"
   >
     <slot />
@@ -16,16 +16,22 @@ export default defineComponent({
   name: 'm-button',
   emits: ['click'],
   props: {
-    type: {
+    size: {
       type: String,
       default: 'base',
+      validator: (size: string) =>
+        ['xs', 'sm', 'base', 'lg', 'xl'].includes(size),
+    },
+    type: {
+      type: String,
+      default: 'primary',
       validator: (type: string) =>
-        ['xs', 'sm', 'base', 'lg', 'xl'].includes(type),
+        ['primary', 'secondary', 'white'].includes(type),
     },
   },
   computed: {
-    typeClasses(): string {
-      switch (this.type) {
+    sizeClasses(): string {
+      switch (this.size) {
         case 'xs':
           return 'px-2.5 py-1.5 text-xs';
         case 'sm':
@@ -36,6 +42,16 @@ export default defineComponent({
           return 'px-6 py-3 text-base';
         default:
           return 'px-4 py-2 text-sm';
+      }
+    },
+    typeClasses(): string {
+      switch (this.type) {
+        case 'secondary':
+          return 'text-blue-700 bg-blue-100 hover:bg-blue-200 focus:ring-blue-500';
+        case 'white':
+          return 'border-gray-300 text-gray-700 bg-white hover:bg-gray-50 focus:ring-blue-500';
+        default:
+          return 'text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500';
       }
     },
   },
